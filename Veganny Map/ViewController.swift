0f6d525bd186895651e0eb4ed80c172e0341561b
Hailey <aojprock@gmail.com>
2022-10-28 18:21:32 +0800

    feat: Add gitignore

---
+++ Veganny Map/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Veganny Map
+//
+//  Created by Hailey on 2022/10/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

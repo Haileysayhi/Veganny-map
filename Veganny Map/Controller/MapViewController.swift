be90023af7cffa4b05a659cbbfff28e2acf17695
Hailey <aojprock@gmail.com>
2022-11-01 15:59:53 +0800

    feat: Add Google Map Marker Clustering
    1. Create Google Map Marker Cluster to vegan restaurants and show in mapView.

--- Veganny Map/Controller/MapViewController.swift
+++ Veganny Map/Controller/MapViewController.swift
@@ -8,19 +8,21 @@
 import UIKit
 import GoogleMaps
 import GooglePlaces
+import GoogleMapsUtils
 import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, GMSMapViewDelegate {
     
     // MARK: - IBOutlet
     @IBOutlet weak var mapView: GMSMapView!
     
     // MARK: - Properies
     let manager = CLLocationManager()
-    var camera = GMSCameraPosition()
+//    var camera = GMSCameraPosition()
     var listResponse: ListResponse?
+    var clusterManager: GMUClusterManager!
     // MARK: - TODO Set user location or Just show all of vegan
-    var location = "25.038456876465034,121.53288929543649" // 座標預設為台北市
+    var location = ""
     
     // MARK: - viewDidLoad
     override func viewDidLoad() {
@@ -29,27 +31,45 @@ class MapViewController: UIViewController {
         manager.requestWhenInUseAuthorization() // request user authorize
         manager.distanceFilter = kCLLocationAccuracyNearestTenMeters // update data after move ten meters
         manager.desiredAccuracy = kCLLocationAccuracyBest
-        // Create a GMSCameraPosition that tells the map to display the
-        // coordinate -33.86,151.20 at zoom level 6.
-        camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-        mapView.camera = camera
-//        print("License:\(GMSServices.openSourceLicenseInfo())") // TODO
+        
+//        camera = GMSCameraPosition.camera(withLatitude: 25.038, longitude: 121.532, zoom: 6.0)
+//        mapView.camera = camera
+        //        print("License:\(GMSServices.openSourceLicenseInfo())") 
+        
+        
+        // 生成 Cluster Manager
+        let iconGenerator = GMUDefaultClusterIconGenerator.init(buckets: [99999], backgroundColors: [UIColor.green])
+        let algorithm = GMUNonHierarchicalDistanceBasedAlgorithm()
+        let renderer = GMUDefaultClusterRenderer(mapView: mapView, clusterIconGenerator: iconGenerator)
+        clusterManager = GMUClusterManager(map: mapView, algorithm: algorithm, renderer: renderer)
+        clusterManager.setMapDelegate(self) // Register self to listen to GMSMapViewDelegate events.
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         
         GoogleMapListController.shared.fetchNearbySearch(location: location, keyword: "vegan") { listresponse in
             self.listResponse = listresponse
+            print("==>位置有沒有吃到\(self.location)")
             print("==>\(listresponse)")
             listresponse?.results.forEach({ result in
                 let marker = GMSMarker()
-                marker.position = CLLocationCoordinate2D(latitude: result.geometry.location.lat, longitude: result.geometry.location.lng)
-                marker.title = result.name
+                marker.position = CLLocationCoordinate2D(
+                    latitude: result.geometry.location.lat,
+                    longitude: result.geometry.location.lng
+                )
+                marker.snippet = result.name
                 marker.icon = GMSMarker.markerImage(with: .green)
+                self.clusterManager.add(marker)
+                self.clusterManager.cluster()
                 marker.map = self.mapView
             })
         }
     }
 }
 
-// MARK: - extension
+// MARK: - CLLocationManagerDelegate
 extension MapViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let currentLocation: CLLocation = locations[0] as CLLocation
@@ -57,6 +77,9 @@ extension MapViewController: CLLocationManagerDelegate {
             mapView.animate(toLocation: location.coordinate)
             mapView.animate(toZoom: 15)
             manager.stopUpdatingLocation()
+            
+            print("目前位置為\n經度為\(location.coordinate.longitude)\n緯度為\(location.coordinate.latitude)")
+            self.location = "\(location.coordinate.latitude),\(location.coordinate.longitude)"
         }
     }
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
@@ -80,3 +103,24 @@ extension MapViewController: CLLocationManagerDelegate {
         }
     }
 }
+
+// MARK: - GMUClusterManagerDelegate
+extension MapViewController: GMUClusterManagerDelegate {
+    
+    func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
+      // center the map on tapped marker
+      mapView.animate(toLocation: marker.position)
+      // check if a cluster icon was tapped
+      if marker.userData is GMUCluster {
+        // zoom in on tapped cluster
+        mapView.animate(toZoom: mapView.camera.zoom + 1)
+        print("Did tap cluster")
+        return true
+      }
+
+        print("Did tap a normal marker")
+      return false
+    }
+}
+
+
